98d95586e29cdad9ecdf976306ab199abe56cc80
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-04-13 06:28:04 -0300

    Update title and tagline in docusaurus.config.ts

--- polvo-PI/docusaurus.config.ts
+++ polvo-PI/docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Polvo PI',
+  tagline: 'Contando e aprendendo com você! 🦑',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
