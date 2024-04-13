24f086772b3f4e55e28b52f92e899a549283eea1
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-04-13 08:18:13 -0300

    Update copyright notice in docusaurus.config.ts

--- polvo-PI/docusaurus.config.ts
+++ polvo-PI/docusaurus.config.ts
@@ -121,7 +121,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Projeto de conclusão (BES) Bacharelado em Engenharia de Software, Ucsal.`,
     },
     prism: {
       theme: prismThemes.github,
