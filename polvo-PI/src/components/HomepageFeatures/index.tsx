a987c622c77655f41f6b856b8a323bf5025b7585
Cristiano Filho ☁️ <54041918+CristianoFIlho@users.noreply.github.com>
2024-04-13 13:52:34 -0300

    Update homepage feature titles and descriptions

--- polvo-PI/src/components/HomepageFeatures/index.tsx
+++ polvo-PI/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Intuitivo e simples de usar',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Experimente a simplicidade que liberta: Uma plataforma intuitiva para você se concentrar no que realmente importa.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Matenha o foco nas suas aulas, envolvendo os alunos com a gamificação',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Transforme sua sala de aula em um playground de aprendizado: Descubra o poder da gamificação, revolucione sua forma de ensinar.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Use o poder da gamificação em sala de aula',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Libere o potencial da sua sala de aula: Utilize a gamificação para transformar o aprendizado em uma experiência inesquecível.
       </>
     ),
   },
